20b9186d824a163eddb3f205bfff7912a0ae382e
Alexandros Metaxas <metaxas.alexan@gmail.com>
2023-08-17 01:24:13 +0300

    Install new font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js}'],
 	darkMode: "class",
@@ -7,6 +9,9 @@ module.exports = {
 			colors: {
 				primary: "#3f4756",
 				secondary: "#ffedcb",
+			},
+			fontFamily: {
+				sans: ['Comfortaa', ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
